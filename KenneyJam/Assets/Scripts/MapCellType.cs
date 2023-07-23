def65a582a2b2b73042668b31a03a14636bb25cf
Phantom <1224983495@qq.com>
2023-07-23 09:30:51 +0800

    地图相关

---
+++ KenneyJam/Assets/Scripts/MapCellType.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public enum MapCellType
+{
+    //block type
+    rock,
+    bedrock,
+    mineral,
+    air,
+    //room type
+    notRoom,
+    emptyRoom,
+    monsterRoom,
+    mineralRoom,
+    turretRoom,
+    strongholdRoom,
+    //error
+    outOfRange,
+}
+
+
---
+++ KenneyJam/Assets/Scripts/MapGetTest.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGetTest : MonoBehaviour
+{
+    [SerializeField]
+    float velocity=20;
+    [SerializeField]
+    MapController mapController;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mapController.DestroyCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(mapController.GetRoomType(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            Debug.Log(mapController.GetBlockType(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            mapController.GenerateMap();
+        }
+        Vector3 pos=Camera.main.transform.position;
+        if(Input.GetKey(KeyCode.W))
+        {
+            Camera.main.transform.position=new Vector3(pos.x,pos.y+velocity*Time.deltaTime,pos.z);
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            Camera.main.transform.position=new Vector3(pos.x,pos.y-velocity*Time.deltaTime,pos.z);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            Camera.main.transform.position=new Vector3(pos.x+velocity*Time.deltaTime,pos.y,pos.z);
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            Camera.main.transform.position=new Vector3(pos.x-velocity*Time.deltaTime,pos.y,pos.z);
+        }
+    }
+}
